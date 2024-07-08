e965ef0666fa452571544417eb0e3f946a8e4fb4
Captain-Kim <brighthero@kakao.com>
2024-07-09 02:51:39 +0900

    chore: 에러로 인한 공통 레이아웃 - 헤더 컴포넌트 임시 주석 처리

--- src/app/(provider)/(root)/layout.tsx
+++ src/app/(provider)/(root)/layout.tsx
@@ -1,10 +1,10 @@
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import { PropsWithChildren } from "react";
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
     <div className="max-w-[1920px] min-h-screen">
-      <Header></Header>
+      {/* <Header></Header> */}
       <main className="w-full max-w-[1280px] mx-auto">{children}</main>
     </div>
   );
