905ecda41dd366f17701bd8b1591e8fd8c3186d9
reeny404 <happy2hyeon@kakao.com>
2024-07-12 12:19:30 +0900

    style : 중복 text 색상 제거 및 더보기 Link로 변경 - 더보기 클릭 시 게시글 목록으로 가도록 변경

--- src/components/Mainpage/BestInfo.tsx
+++ src/components/Mainpage/BestInfo.tsx
@@ -52,7 +52,9 @@ const BestInfo: FC = () => {
           <p className="text-25px">금주의 베스트 게시글 입니다.</p>
         </div>
         <Link href="/posts" className="no-underline">
-          <p className="text-gray-500 text-sm text-[#2e2e2e]">더보기 &gt;</p>
+          <Link href="/posts" className="text-sm text-font-color">
+            더보기 &gt;
+          </Link>
         </Link>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
@@ -63,34 +65,36 @@ const BestInfo: FC = () => {
               href={`/posts/${post.post_id}`}
               legacyBehavior
             >
-              <a className="relative rounded-lg overflow-hidden block">
-                <img
-                  src={post.image}
-                  alt={post.title}
-                  className="w-[600px] h-[400px] rounded-2xl"
+              <div className="relative rounded-lg overflow-hidden block">
+                <Image
+                  src={post.image ?? "/post/fallback.svg"}
+                  alt={post.title ?? "게시글 첨부 이미지"}
+                  width="600"
+                  height="400"
+                  className="min-h-[400px] rounded-2xl object-cover aspect-auto"
                 />
                 <div className="p-4">
                   <h3 className="text-lg font-semibold truncate-2-lines text-main-color">
                     {post.title}
                   </h3>
-                  <p className="text-gray-600 mt-1 truncate-3-lines text-[#2e2e2e]">
+                  <p className="mt-1 truncate-3-lines text-font-color">
                     {post.content}
                   </p>
                   <div className="flex justify-between items-center mt-4">
-                    <span className="text-gray-500 text-sm text-[#2e2e2e]">
+                    <span className="text-sm text-font-color">
                       작성자 {post.author_nickname}
                     </span>
-                    <span className="text-gray-500 text-sm text-[#2e2e2e]">
+                    <span className="text-sm text-font-color">
                       {new Date(post.created_at).toLocaleDateString()}
                     </span>
                   </div>
                   <div className="flex justify-between items-center mt-2">
-                    <span className="text-green-600 text-sm text-[#2e2e2e]">
+                    <span className="text-green-600 text-sm">
                       ♥ {post.likes ?? 0}
                     </span>
                   </div>
                 </div>
-              </a>
+              </div>
             </Link>
           ))
         ) : (
